10dfe5e9faabe241e852ddbab41afc428bc0f407
Maverickspb78 <Maverickspb78@gmail.com>
2021-06-07 11:04:26 +0300

    fixes

--- src/main/java/com/andreev/demoshop/domain/Bucket.java
+++ src/main/java/com/andreev/demoshop/domain/Bucket.java
@@ -25,7 +25,7 @@ public class Bucket {
     @JoinColumn(name = "user_id")
     private User user;
     @ManyToMany
-    @JoinTable(name = "buckets_products",
+    @JoinTable(name = "bucket_products",
             joinColumns = @JoinColumn(name = "bucket_id"),
             inverseJoinColumns = @JoinColumn(name = "product_id"))
     private List<Product> products;
--- src/main/resources/db/migration/V2__add_admin.sql
+++ src/main/resources/db/migration/V2__add_admin.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (id, archive, name, password, role, email, bucket_id)
-VALUES (1, false, 'admin', 'pass', 'ADMIN', 'mail@mail.ru', null);
+VALUES (1, false, 'admin', '$2a$10$6vU7AsXGozUiU95WsEUlQOE8dSjhrbSuuIXdUSRuYBTP95unc3Wzi', 'ADMIN', 'mail@mail.ru', null);
 
 ALTER SEQUENCE user_seq RESTART WITH 2;
