ba8f3d98bf7014581eeace070a15cda3b18337bd
Maverickspb78 <Maverickspb78@gmail.com>
2021-06-29 11:52:55 +0300

    HW6

--- src/main/java/com/andreev/demoshop/service/OrderService.java
+++ src/main/java/com/andreev/demoshop/service/OrderService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface OrderService {
     List<Order> getAll();
     void saveOrder(Order order);
+    Order getOrder(Long id);
 }
--- src/main/java/com/andreev/demoshop/service/OrderServiceImpl.java
+++ src/main/java/com/andreev/demoshop/service/OrderServiceImpl.java
@@ -1,22 +1,27 @@
 package com.andreev.demoshop.service;
 
 
+import com.andreev.demoshop.config.OrderIntegrationConfig;
 import com.andreev.demoshop.dao.OrderRepository;
 import com.andreev.demoshop.domain.Order;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.andreev.demoshop.dto.OrderIntegrationDto;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService{
 
-    OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+    private final OrderIntegrationConfig integrationConfig;
 
-    @Autowired
-    public void setOrderRepository(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, OrderIntegrationConfig integrationConfig) {
         this.orderRepository = orderRepository;
+        this.integrationConfig = integrationConfig;
     }
 
     @Override
@@ -27,6 +32,28 @@ public class OrderServiceImpl implements OrderService{
     @Override
     @Transactional
     public void saveOrder(Order order) {
-        orderRepository.save(order);
+        Order savedOrder = orderRepository.save(order);
+        sendIntegrationNotify(savedOrder);
+    }
+
+    private void sendIntegrationNotify(Order order){
+        OrderIntegrationDto dto = new OrderIntegrationDto();
+        dto.setUsername(order.getUser().getName());
+        dto.setAddress(order.getAddress());
+        dto.setOrderId(order.getId());
+        List<OrderIntegrationDto.OrderDetailsDto> details = order.getDetails().stream()
+                .map(OrderIntegrationDto.OrderDetailsDto::new).collect(Collectors.toList());
+        dto.setDetails(details);
+
+        Message<OrderIntegrationDto> message = MessageBuilder.withPayload(dto)
+                .setHeader("Content-type", "application/json")
+                .build();
+
+        integrationConfig.getOrdersChannel().send(message);
+    }
+
+    @Override
+    public Order getOrder(Long id) {
+        return orderRepository.findById(id).orElse(null);
     }
 }
