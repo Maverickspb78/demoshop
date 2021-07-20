860be97c2f627fc5de9659820e3f2498b1636c7a
Maverickspb78 <Maverickspb78@gmail.com>
2021-07-20 11:38:56 +0300

    fix 7

--- src/main/resources/db/migration/V2__add_admin.sql
+++ src/main/resources/db/migration/V2__add_admin.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id, archive, email, name, password, role)
-VALUES (1, false, 'mail@mail.ru', 'admin', 'pass', 'ADMIN');
+INSERT INTO users (id, archive, email, name, password, role, bucket_id)
+values (1, false, 'mail@mail.ru', 'admin', 'pass', 'ADMIN', null);
 
 ALTER SEQUENCE user_seq RESTART WITH 2;
